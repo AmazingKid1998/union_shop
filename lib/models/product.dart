392d3e34111b58b938435f2d12ee18693796a192
AmazingKid1998 <sheium1998@gmail.com>
2025-11-28 03:02:29 +0000

    update product model to have collection inside of it

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,8 +2,9 @@ class Product {
   final String id;
   final String title;
   final double price;
-  final String image; // We will use a placeholder URL
+  final String image;
   final String description;
+  final String collectionId; // <--- NEW ATTRIBUTE
 
   Product({
     required this.id,
@@ -11,12 +12,6 @@ class Product {
     required this.price,
     required this.image,
     required this.description,
+    required this.collectionId, // Required now
   });
-}
-class Collection {
-  final String id;
-  final String title;
-  final String image;
-
-  Collection({required this.id, required this.title, required this.image});
 }
