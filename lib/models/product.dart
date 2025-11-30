dfaa6994003c851f552b84112eb327f29afd6e6f
AmazingKid1998 <sheium1998@gmail.com>
2025-11-30 19:26:15 +0000

    update product model to have oldprice for sale items

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,15 +1,17 @@
 class Product {
   final String id;
   final String title;
-  final double price;
+  final double price; // The current (sale) price
+  final double? oldPrice; // The original price (Null if not on sale)
   final String image;
   final String description;
-  final String collectionId; // Used for filtering categories
+  final String collectionId;
 
   Product({
     required this.id,
     required this.title,
     required this.price,
+    this.oldPrice, // Optional
     required this.image,
     required this.description,
     required this.collectionId,
