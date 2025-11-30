4dabe0112f8055d330ae3f0de52c9a83c2a3acc4
AmazingKid1998 <sheium1998@gmail.com>
2025-11-30 19:35:47 +0000

    import sale route to main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
- // Keep this if you are using provider, otherwise remove
-// Import all your pages
 import 'pages/home_page.dart';
 import 'pages/about_page.dart';
 import 'pages/collections_page.dart';
 import 'pages/print_shack_page.dart';
 import 'pages/cart_page.dart';
 import 'pages/login_page.dart';
+import 'pages/sale_page.dart'; // Import Sale Page
 
 void main() {
   runApp(const UnionShopApp());
@@ -28,7 +27,7 @@ class UnionShopApp extends StatelessWidget {
       // 1. Set the initial route
       initialRoute: '/',
       
-      // 2. Define the Named Routes
+      // 2. Define ALL Named Routes
       routes: {
         '/': (context) => const HomePage(),
         '/about': (context) => const AboutPage(),
@@ -36,6 +35,7 @@ class UnionShopApp extends StatelessWidget {
         '/print-shack': (context) => const PrintShackPage(),
         '/cart': (context) => const CartPage(),
         '/login': (context) => const LoginPage(),
+        '/sale': (context) => const SalePage(), // <--- Added Sale Route
       },
     );
   }
