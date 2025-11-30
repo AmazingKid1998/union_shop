e0d9a71dddf390b99ce9682cd4d7b31c2a5ec475
AmazingKid1998 <sheium1998@gmail.com>
2025-11-30 19:38:02 +0000

    update sale page route

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'pages/collections_page.dart';
 import 'pages/print_shack_page.dart';
 import 'pages/cart_page.dart';
 import 'pages/login_page.dart';
-import 'pages/sale_page.dart'; // Import Sale Page
+import 'pages/sale_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -23,11 +23,7 @@ class UnionShopApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
         useMaterial3: true,
       ),
-      
-      // 1. Set the initial route
       initialRoute: '/',
-      
-      // 2. Define ALL Named Routes
       routes: {
         '/': (context) => const HomePage(),
         '/about': (context) => const AboutPage(),
@@ -35,7 +31,7 @@ class UnionShopApp extends StatelessWidget {
         '/print-shack': (context) => const PrintShackPage(),
         '/cart': (context) => const CartPage(),
         '/login': (context) => const LoginPage(),
-        '/sale': (context) => const SalePage(), // <--- Added Sale Route
+        '/sale': (context) => const SalePage(),
       },
     );
   }
