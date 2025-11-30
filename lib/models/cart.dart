59896a418ebc3f225825f50a93f5e7564e4ffc29
AmazingKid1998 <sheium1998@gmail.com>
2025-11-30 16:04:55 +0000

    add comments to cart model

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,9 +1,9 @@
 import 'product.dart';
 
-// Just a simple global list
+// Global cart list accessed by all pages
 List<Product> globalCart = [];
 
-// A simple helper to get the total
+// Helper to calculate total price
 double getCartTotal() {
   return globalCart.fold(0, (total, current) => total + current.price);
 }
