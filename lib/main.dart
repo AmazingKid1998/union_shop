7a4ca6a0fa0535cc1637d391d2157a652480b040
AmazingKid1998 <sheium1998@gmail.com>
2025-12-01 19:52:59 +0000

    add new print shack routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // REQUIRED FOR MVVM ARCHITECTURE
+import 'package:provider/provider.dart';
 
-// MVVM Imports
+// ViewModels
 import 'viewmodels/shop_viewmodel.dart';
 import 'viewmodels/cart_viewmodel.dart';
 
-// View Imports
+// Pages
 import 'pages/home_page.dart';
 import 'pages/about_page.dart';
 import 'pages/collections_page.dart';
-import 'pages/print_shack_page.dart';
+import 'pages/print_shack_page.dart'; // The Tool
+import 'pages/print_shack_menu_page.dart'; // The Menu
+import 'pages/print_shack_about_page.dart'; // The Info
 import 'pages/cart_page.dart';
 import 'pages/login_page.dart';
 import 'pages/sale_page.dart';
 
 void main() {
   runApp(
-    // Inject ViewModels here
     MultiProvider(
       providers: [
-        // Shop data and filtering logic
         ChangeNotifierProvider(create: (_) => ShopViewModel()),
-        // Cart state management logic
         ChangeNotifierProvider(create: (_) => CartViewModel()),
       ],
       child: const UnionShopApp(),
@@ -46,11 +45,15 @@ class UnionShopApp extends StatelessWidget {
         '/': (context) => const HomePage(),
         '/about': (context) => const AboutPage(),
         '/shop': (context) => const CollectionsPage(),
-        '/collection':(context)=>const CollectionsPage(),
-        '/print-shack': (context) => const PrintShackPage(),
+        '/collection':(context)=> const CollectionsPage(),
         '/cart': (context) => const CartPage(),
         '/login': (context) => const LoginPage(),
         '/sale': (context) => const SalePage(),
+        
+        // --- PRINT SHACK ROUTES ---
+        '/print-shack': (context) => const PrintShackMenuPage(), // Main Landing
+        '/print-shack/tool': (context) => const PrintShackPage(), // The Editor
+        '/print-shack/about': (context) => const PrintShackAboutPage(), // The Info
       },
     );
   }
