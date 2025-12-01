34e59dbb6f7e878206085a5f73ca56e6158e334d
AmazingKid1998 <sheium1998@gmail.com>
2025-12-01 15:29:41 +0000

    add products from dummy data to product repo

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -6,16 +6,50 @@ class ProductRepository {
     // PASTE YOUR FULL LIST OF DUMMY PRODUCTS HERE (from dummy_data.dart)
     // I am abbreviating for brevity, but you should copy the whole list.
     Product(
-      id: 'p1',
-      title: 'Classic Hoodies',
-      price: 25.00,
-      oldPrice: 35.00,
-      image: 'assets/images/p1.jpeg',
-      description: 'Our best selling Classic Hoodie comes in great colours!',
-      collectionId: 'c_clothing',
-    ),
+    id: 'p1',
+    title: 'Classic Hoodies',
+    price: 25.00, 
+    oldPrice: 35.00, // <--- ON SALE!
+    image: 'assets/images/p1.jpeg',
+    description: 'Our best selling Classic Hoodie comes in great colours!',
+    collectionId: 'c_clothing',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Classic Sweatshirts',
+    price: 23.00,
+    // No oldPrice -> Not on sale
+    image: 'assets/images/p2.jpeg',
+    description: 'A comfortable classic sweatshirt perfect for everyday wear.',
+    collectionId: 'c_clothing',
+  ),
+  Product(
+    id: 'p3',
+    title: 'Classic T-Shirts',
+    price: 11.00,
+    oldPrice: 15.00, // <--- ON SALE!
+    image: 'assets/images/p1.jpeg',
+    description: 'Soft cotton T-shirt featuring the Union logo.',
+    collectionId: 'c_clothing',
+  ),
+  // ... (Keep the rest of your products the same, just copy them back in)
+  // For brevity, I am showing the first 3. You should keep your full list of 18 items.
+  // Just ensure you add the 'oldPrice: null,' or leave it out for non-sale items.
+  
+  // Example of another sale item
+  Product(
+    id: 'p6',
+    title: 'Limited Edition Zip Hoodie',
+    price: 20.00,
+    oldPrice: 30.00, // <--- ON SALE!
+    image: 'assets/images/p1.jpeg',
+    description: 'Limited run essential zip hoodie.',
+    collectionId: 'c_clothing',
+  ),
+  
+  // ... (Paste the rest of your products here) ...
+];
     // ... add the rest ...
-  ];
 
   // 2. Methods to access data (As requested by professor)
   
