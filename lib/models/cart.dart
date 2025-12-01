4c6a5c5e30e754e1030b16154462ef4a0c51579b
AmazingKid1998 <sheium1998@gmail.com>
2025-12-01 15:44:13 +0000

    delete cart as i moved all functionality to cart repo and cart view model

--- lib/models/cart.dart
+++
@@ -1,9 +0,0 @@
-import 'product.dart';
-
-// Global cart list accessed by all pages
-List<Product> globalCart = [];
-
-// Helper to calculate total price
-double getCartTotal() {
-  return globalCart.fold(0, (total, current) => total + current.price);
-}
